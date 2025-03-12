6e54483bdc1cf99054a57a2aca5d4dfae1acace7
Anastasia Zabolotnay <anastasiabuse916@gmail.com>
2025-03-12 21:49:44 +0200

    Update cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,6 @@
-export const cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) ||
+
+[{
     productId: '77a845b1-16ed-4eac-bdf9-5b591882113d',
     quantity: 2 
 }, {
@@ -6,6 +8,10 @@ export const cart = [{
     quantity: 1
 }];                                                                 
 //Метод с ID - D-Dyplicated the date or Normalizing the data
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
  export function addToCart(productId) {
     let matchingItem = cart.find(item => item.productId === productId);
@@ -18,4 +24,19 @@ export const cart = [{
                 quantity: 1
             });
         }
+
+        saveToStorage();
+}
+
+export function removeFromCart(productId) {
+    const newCart = [];
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
+
+    saveToStorage();
 }
